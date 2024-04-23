03b34970418b2df738ae0ef10a1675762408658f
victorien <victorienmichel3@gmail.com>
2024-04-23 13:53:08 +0200

    [FIX]: fix print data and residuals

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,8 +1,7 @@
 import sys
-from pathlib import Path
-
 sys.path.append(__file__.split("tests")[0])
 
+from pathlib import Path
 from utils.fetch import fetch_postprocessing_data, fetch_residuals
 
 def main():
@@ -10,14 +9,14 @@ def main():
     project_path = "/home/victorien/openfoam9/motorBike"
     run_id = "run002"
     run_path = Path(f"{project_path}/{run_id}")
-
     
     # Fetch the postprocessing data
     data = fetch_postprocessing_data(run_path, directory='probes')
-    
-    # Print the data
-    for d in data:
-        print(data[d])
+    print(data)
+
+    # Fetch the residuals
+    residuals = fetch_residuals(run_path)
+    print(residuals)
 
 if __name__ == "__main__":
     main()

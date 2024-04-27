b61408e0928e9a65f7440c90b0fbcb5dfe7fb4f9
victorien <victorienmichel3@gmail.com>
2024-04-28 00:07:14 +0200

    [FIX]: columns are now inputs to figure_in_tab

--- utils/plot.py
+++ utils/plot.py
@@ -1,22 +1,16 @@
 import plotly.graph_objs as go
 import streamlit as st
 
-def figure_in_tab(
-        dataset, logscale=False, preselect_all=False, ylabel=None, title=''
-    ):
-    # Select the fields to plot
-    selected_columns = st.multiselect(
-        'Select the fields to plot',
-        dataset.columns,
-        default=dataset.columns.tolist() if preselect_all else None
-    )
 
+def figure_in_tab(
+        dataset, columns, logscale=False, ylabel=None, title=''
+    ): 
     # Plot the fields
     traces = []
-    for col in selected_columns:
+    for col in columns:
         trace = go.Scatter(
-            x=dataset[selected_columns].index,
-            y=dataset[selected_columns][col],
+            x=dataset[columns].index,
+            y=dataset[columns][col],
             mode='lines',
             name=col,
         )
